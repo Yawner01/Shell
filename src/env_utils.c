e36daf6d54bf882308dde5446e227bd5b9885c0d
Yawner01 <wesallen111@gmail.com>
2024-09-17 21:37:42 -0400

    Added part 5

--- src/env_utils.c
+++ src/env_utils.c
@@ -68,9 +68,43 @@ void search_path(char *command) {
         if (access(full_path, X_OK) == 0) {
             printf("Command found: %s\n", full_path);
 
-            //char *args[] = {full_path, NULL};
-            //need to figure out how to fork and make new process
-            // Part 5 starts here
+            //fork
+            pid_t pid = fork();
+            if (pid < 0) {
+                fprintf(stderr, "Fork failed\n"); //verify its not negative
+                exit(EXIT_FAILURE);
+            } 
+            else if (pid == 0) { //child process
+                char *args[100]; //this allows for multiple arguments like the ls -al
+                int i = 0;
+
+                //here i am going to split the command into tokens and store them
+                char *token = strtok(command, " ");
+                while (token != NULL) {
+                    args[i++] = token; //storing
+                    token = strtok(NULL, " ");
+                }
+                args[i] = NULL; //null terminator
+
+                execv(full_path, args); //execute command with arguments 
+
+                fprintf(stderr, "Error executing: %s\n", full_path);//failure notif if it doesnt work
+                exit(EXIT_FAILURE);
+                
+            } 
+            else { //parent
+                //here is where the process waits for
+                //the child process to complete
+                int status;
+                waitpid(pid, &status, 0);
+                if (WIFEXITED(status)) { 
+                    printf("Child exited with status %d\n", WEXITSTATUS(status)); //failure notif if it doesnt work
+                }
+
+            }
+
+            
+            free(path_copy);
             return;
         }
 
