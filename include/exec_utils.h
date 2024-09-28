14ed9907af03ab379d0acdc2edd5d9352290f67e
Jose Rodriguez <cubanmix2u@yahoo.com>
2024-09-28 00:09:08 -0400

    updated execute_commands() function

--- include/exec_utils.h
+++ include/exec_utils.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "lexer.h"
+#include "commands.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 tokenlist **parse_pipes(tokenlist *tokens, int *num_cmds);
-void execute_commands(tokenlist **commands, int num_cmds);
+void execute_commands(tokenlist **commands, int num_cmds, tokenlist* command_history, job_t* jobs, int num_jobs);
